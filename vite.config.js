6d26d436d2f692f82bdfcc5098a72ee718afa14e
SANVEE <samsanvee7@gmail.com>
2026-05-06 04:22:16 -0400

    debug du site4

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        quiz: path.resolve(__dirname, 'quiz.html'),
-        accueil: path.resolve(__dirname, 'accueil.html'),
-      },
-    },
-  },
+        main: 'index.html',
+        quiz: 'quiz.html',
+        accueil: 'accueil.html'
+      }
+    }
+  }
 })
