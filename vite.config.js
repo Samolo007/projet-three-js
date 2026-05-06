7681692a98a998dd4cbb48cd06290c3ebda1c6f4
SANVEE <samsanvee7@gmail.com>
2026-05-06 04:10:55 -0400

    debug du site

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        quiz: resolve(__dirname, 'quiz.html'),
+        accueil: resolve(__dirname, 'accueil.html') // Vu que tu as aussi un accueil.html
+      },
+    },
+  },
+})
